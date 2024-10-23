5610592090e859135db2e7d9fa69300c767d3388
Ben Porter <benjporter1511@gmail.com>
2024-10-23 13:25:20 +0100

    adding functionality from tech test

--- app/routes/_layout.search_.results.$message_id.tsx
+++ app/routes/_layout.search_.results.$message_id.tsx
@@ -1,7 +1,8 @@
 import { type LoaderFunctionArgs } from '@remix-run/node'
 import { json, useLoaderData, useParams } from '@remix-run/react'
+import { useState } from 'react'
 import { requireSession } from '~/lib/auth'
-import { api } from '~/lib/utils'
+import { api, API_URL } from '~/lib/utils'
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
   const { session } = await requireSession(request)
@@ -23,6 +24,33 @@ export default function Message() {
   if (!params.message_id) {
     return <div></div>
   }
+  const [status, setStatus] = useState('')
+  const updateStatus = async () => {
+    // for testing
+    // console.log("Message ID", params.message_id)
+    // console.log('Sending payload:', JSON.stringify({ status }))
+    try {
+      const response = await fetch(API_URL + `/messages/${params.message_id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',  // Add this line
+        },
+        body: JSON.stringify({ status }),
+        credentials: 'include'
+      })
+      //console.log(response)
+
+      if (!response.ok) {
+        throw new Error('Failed to update status');
+      }
+
+      setStatus('')
+      // added this due to page needing a refesh upon update
+      window.location.reload()
+    } catch (error) {
+      console.error('Error updating status:', error);
+    }
+  }
 
   return (
     <div className='relative h-full'>
@@ -35,7 +63,18 @@ export default function Message() {
               </h2>
               <p className='text-sm text-gray-500'>{message.from_email}</p>
             </div>
-
+            <p>Status: {message.status}</p>
+            <input 
+                type="text" 
+                value={status} 
+                onChange={(e) => setStatus(e.target.value)} 
+                placeholder="Enter new status" 
+            />
+            <button
+              type='submit'
+              className='flex justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600'
+              onClick={updateStatus}>Update Status
+            </button>
             <p>{message.content}</p>
           </div>
         </div>
--- app/routes/_layout.search_.results.tsx
+++ app/routes/_layout.search_.results.tsx
@@ -2,6 +2,7 @@ import { type LoaderFunctionArgs, json } from '@remix-run/node'
 import { Outlet, useLoaderData, useSearchParams } from '@remix-run/react'
 import { requireSession } from '~/lib/auth'
 import { api } from '~/lib/utils'
+import { useState } from 'react'
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const { session } = await requireSession(request)
@@ -22,6 +23,17 @@ export default function SearchResults() {
   // @ts-ignore
   const { messages } = useLoaderData()
   const [searchParams] = useSearchParams()
+  const [currentPage, setCurrentPage] = useState(1)
+  const messagesPerPage = 10
+  const numberOfPages = Math.ceil(messages.length / messagesPerPage)
+
+  const lastIndex = currentPage * messagesPerPage
+  const firstIndex = lastIndex - messagesPerPage
+  const currentMessages = messages.slice(firstIndex, lastIndex)
+
+  const setPage = (pageNumber: number) => {
+    setCurrentPage(pageNumber)
+  }
 
   return (
     <div className='py-4'>
@@ -42,6 +54,12 @@ export default function SearchResults() {
                     }}
                   />
                 </p>
+                <p className='text-sm text-gray-500'>
+                  Result count: {messages.length}
+                </p>
+                <p className='text-sm text-gray-500'>
+                  Showing Results {firstIndex} - {lastIndex}
+                </p>
               </div>
 
               <div className='flex space-x-4'>
@@ -49,8 +67,8 @@ export default function SearchResults() {
                   role='list'
                   className='w-2/5 flex-shrink-0 divide-y divide-gray-100 overflow-scroll bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl'
                 >
-                  {messages.length &&
-                    messages.map((message: any) => {
+                  {messages &&
+                    currentMessages.map((message: any) => {
                       return (
                         <li className='relative flex justify-between gap-x-6 px-3 py-4 hover:bg-gray-50 sm:px-6'>
                           <div className='flex min-w-0 gap-x-4'>
@@ -85,6 +103,13 @@ export default function SearchResults() {
                 <div className='h-full w-full'>
                   <Outlet />
                 </div>
+              </div>
+              <div className='flex items-center justify-center h-full ring-1 ring-gray-900/5 rounded-xl shadow'>
+                {Array(numberOfPages).fill(0).map((_, i) => (
+                  <button className="w-32 h-12 px-4 py-2 text-lg rounded shadow" onClick={() => setPage(i + 1)}>
+                    {i + 1}
+                  </button>
+                ))}
               </div>
             </div>
           </div>
